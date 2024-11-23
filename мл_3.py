08dac528c89dce0986f296bdec2ff96d6d39703b
Daryna Shmyga <159772550+lldarria@users.noreply.github.com>
2024-11-23 14:42:59 +0200

    Update мл_3.py

--- мл_3.py
+++ мл_3.py
@@ -1,19 +1,24 @@
 import re
 
-phone_numbers = [
-"    +38(050)123-32-34"
-"     0503451234"
-"(050)8889900"
-"38050-111-22-22"
-"38050 111 22 11 mno  "
-]
+def normalize_phone(phone_number):
+    # Видаляємо всі зайві символи, залишаючи лише цифри та '+'
+    phone_number = re.sub(r'[^\d+]', '', phone_number.strip())
+    
+    if phone_number.startswith('+38'):
+        return phone_number
+    elif phone_number.startswith('+'):
+        # Якщо номер починається з '+', додаємо код '+38', але уникаємо дублювання
+        return '+38' + phone_number[1:]
+    elif phone_number.startswith('380'):
+        # Якщо номер починається з '380', додаємо тільки '+'
+        return '+' + phone_number
+    else:
+        # Якщо код країни відсутній, додаємо '+38'
+        return '+38' + phone_number
 
-for phone in phone_numbers:
-    cleaned_number= re.sub(r'\D', ' ', phone)
-    if not cleaned_number.startswith("+"):
-        if cleaned_number.startswith('380'):
-            cleaned_number = '+' + cleaned_number
-        else:
-            cleaned_number = '+38 ' + cleaned_number
-
-print(phone_numbers)
+# Тестування функції
+print(normalize_phone("    +38(050)123-32-34"))  # +380501233234
+print(normalize_phone("     0503451234"))        # +380503451234
+print(normalize_phone("(050)8889900"))           # +380508889900
+print(normalize_phone("38050-111-22-22"))        # +380501112222
+print(normalize_phone("38050 111 22 11   "))     # +380501112211
