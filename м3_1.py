17f8f80e4e843e5d4d9a3923646b37e3221a0435
Daryna Shmyga <159772550+lldarria@users.noreply.github.com>
2024-11-23 14:35:49 +0200

    Update м3_1.py

--- м3_1.py
+++ м3_1.py
@@ -1,10 +1,15 @@
 from datetime import datetime
 
-def days_until(target_date_str):
-    target_date = datetime.strptime(target_date_str, "%Y-%m-%d")
-    current_date = datetime.today()
-    difference = (target_date - current_date).days
-    
-    return difference
+def get_days_from_today(date_str):
+    try:
+        target_date = datetime.strptime(date_str, "%Y-%m-%d").date()
+        current_date = datetime.today().date()
+        difference = (current_date - target_date).days
+        return difference
+    except ValueError:
+        return "Error: Invalid date format. Please use 'YYYY-MM-DD'."
 
-print (days_until("2025-01-01"))
+print(get_days_from_today("2025-01-01"))  # Від'ємне число (майбутня дата)
+print(get_days_from_today("2020-01-01"))  # Додатне число (минуле)
+print(get_days_from_today("2024-11-23"))  # 0 (поточна дата)
+print(get_days_from_today("12.2.2000"))   # Повідомлення про помилку
